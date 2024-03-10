26004002f23c8a0e7b26047fb3f7536b661ce100
Edgar Marques <dev@weaccelerategrowth.com>
2024-03-10 21:12:07 +0000

    Clean the Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "gray": '#EFEFEF',
+      "white": '#FAFAFA',
+      'blue': '#5500FF',
+      'blue-dark': '#3F3CBB',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
