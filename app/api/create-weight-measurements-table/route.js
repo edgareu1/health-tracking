fd6f54d8b20d5ca816b693c781ce98af7636a72c
Edgar Marques <dev@weaccelerategrowth.com>
2024-03-10 19:00:43 +0000

    Create the api/create-weight-measurements-table route

---
+++ app/api/create-weight-measurements-table/route.js
@@ -0,0 +1,11 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+	try {
+		const result = await sql`CREATE TABLE WEIGHT_MEASUREMENTS ( date DATE, weight NUMERIC(3,1), body_fat NUMERIC(3,1), body_muscle NUMERIC(3,1) );`;
+		return NextResponse.json({ result }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ error }, { status: 500 });
+	}
+}
